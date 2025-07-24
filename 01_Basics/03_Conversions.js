901fd3c7559ef815dc65c265cf0a8c6732171157
Omkar Tukaram Aghav <137043446+Omkar-Aghav7184@users.noreply.github.com>
2025-07-24 07:02:02 +0000

    Conversions of Datatypes

---
+++ 01_Basics/03_Conversions.js
@@ -0,0 +1,39 @@
+
+let name="33omkar"
+
+// console.log(`The value of name is ${name}`);
+// console.log(`The Type of name is ${typeof(name)}`);
+
+let valueInNumber= Number(name)
+// console.log(`The value of valueNumber is ${valueInNumber}`);
+// console.log(`The Type of valueNumber is ${typeof(valueInNumber)}`);
+
+/* ""-->0; "33"-->33; "33omkar"--->NaN
+true---> 1, false--->0
+null-->0 ; undefined -->NaN*/
+
+let isLoggedIn
+// console.log(`The value of isLoggedIn is ${isLoggedIn}`);
+// console.log(`The Type of isLoggedIn is ${typeof(isLoggedIn)}`);
+
+let valueInBoolean= Boolean(isLoggedIn)
+// console.log(`The value of valueInBoolean is ${valueInBoolean}`);
+// console.log(`The Type of valueInBoolean is ${typeof(valueInBoolean)}`);
+
+// true-->true ; false--->false
+//"omkar"--->true; ""---> false
+//1/7-->true ; 0-->false
+//null-->false ; undefined--->false
+
+let num
+console.log(`The value of num is ${num}`);
+console.log(`The Type of num is ${typeof(num)}`);
+
+let valueINString= String(num)
+console.log(`The value of valueINString is ${valueINString}`);
+console.log(`The Type of valueINString is ${typeof(valueINString)}`);
+
+//18-->18
+//""-->""; "omkar"--->omkar
+//true--->true ; false-->false
+//null----> null ; undefined --->undefined 
