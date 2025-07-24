a85fb03ec41c962fd1c27b48b0ca55d01774ffd3
Omkar Tukaram Aghav <137043446+Omkar-Aghav7184@users.noreply.github.com>
2025-07-24 12:48:16 +0000

    Memory Info

--- 01_Basics/DataTypes_Summary.js
+++ 01_Basics/DataTypes_Summary.js
@@ -14,18 +14,18 @@ let Id=Symbol('123');
 let anotherId=Symbol('123');
 let BigNumber=234567898765432345678n;
 
-console.log("\n Printing values of primitive datatypes");
-console.table([num,name,isLoggedIn,outsideTemp,state,Id,anotherId,BigNumber]);
-console.log("\n Printing Types of primitive datatypes");
-console.log(`The Datatype of num ${typeof(num)}`); //number
-console.log(`The Datatype of name ${typeof(name)}`); //string
-console.log(`The Datatype of isLoggedIn ${typeof(isLoggedIn)}`); //boolean
-console.log(`The Datatype of outSideTemp ${typeof(outsideTemp)}`); //object
-console.log(`The Datatype of state ${typeof(state)}`); //undefined
-console.log(`The Datatype of Id ${typeof(Id)}`); //symbol
-console.log(`The Datatype of anotherId ${typeof(anotherId)}`); //symbol
-console.log(`The Datatype of BigNumber ${typeof(BigNumber)}`); //bigint
-console.log(Id === anotherId); //false
+// console.log("\n Printing values of primitive datatypes");
+// console.table([num,name,isLoggedIn,outsideTemp,state,Id,anotherId,BigNumber]);
+// console.log("\n Printing Types of primitive datatypes");
+// console.log(`The Datatype of num ${typeof(num)}`); //number
+// console.log(`The Datatype of name ${typeof(name)}`); //string
+// console.log(`The Datatype of isLoggedIn ${typeof(isLoggedIn)}`); //boolean
+// console.log(`The Datatype of outSideTemp ${typeof(outsideTemp)}`); //object
+// console.log(`The Datatype of state ${typeof(state)}`); //undefined
+// console.log(`The Datatype of Id ${typeof(Id)}`); //symbol
+// console.log(`The Datatype of anotherId ${typeof(anotherId)}`); //symbol
+// console.log(`The Datatype of BigNumber ${typeof(BigNumber)}`); //bigint
+// console.log(Id === anotherId); //false
 
 // 2.Reference(Non-Primitive): Call by Reference
 //1)Arrays 2)Objects 3)Functions
@@ -40,8 +40,34 @@ let myObj={
 let sum= function sumOfTwoNumbers(num,scoreValue){
     return (num + scoreValue);
 }
-console.log("\nThe Type of Arrays datatype: "+ (typeof(captain))); //object
-console.log("\nThe Type of Object datatype: "+ (typeof(myObj))); //object
-console.log("\nThe Type of Functions datatype: "+ (typeof(sum))); //function(object function)
+// console.log("\nThe Type of Arrays datatype: "+ (typeof(captain))); //object
+// console.log("\nThe Type of Object datatype: "+ (typeof(myObj))); //object
+// console.log("\nThe Type of Functions datatype: "+ (typeof(sum))); //function(object function)
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/************Memory Information**************/
+//1. Stack--->All Primitive DataTypes---> Call By Value---> Copy of var not original value
+//1. Heap-->All Non-Primitive DataTypes---> Call By Reference---> Changes in Original value not copy
+
+let myEmail1="omkar.google@gmail.com";
+let myEmail2=myEmail1;
+console.log(`The value of myEmail1 is: ${myEmail1}`);
+console.log(`The value of myEmail2 is: ${myEmail2}`);
+myEmail2="ota.google@gmail.com";
+console.log(`The value of myEmail1 is: ${myEmail1}`); //not change because it gives copy not original value
+console.log(`The value of myEmail2 is: ${myEmail2}`);
+
+let userOne={
+    email:"user1@gmail.com",
+    age:22
+}
+console.log("\n")
+let userTwo=userOne;
+console.log("The value of userOne is: " + JSON.stringify(userOne) );
+console.log("The value of userTwo is: " + JSON.stringify(userTwo));
+userTwo.email="user2@google.com";
+userTwo.age=27;
+console.log(`The value of userOne ${JSON.stringify(userOne)}`); //Changes in original value 
+console.log(`The value of userTwo ${JSON.stringify(userTwo)}`);
+
