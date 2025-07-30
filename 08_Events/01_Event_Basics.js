263e77a3aff0f180df5276a0a6b31e3a4ac51036
Omkar Tukaram Aghav <137043446+Omkar-Aghav7184@users.noreply.github.com>
2025-07-30 06:52:32 +0000

    Events

---
+++ 08_Events/01_Event_Basics.js
@@ -0,0 +1,11 @@
+console.log("Let's Learn most important concept in JS:- Events");
+
+//Topics to need to study in events in Interview-perpective, project/coding point of view
+        //type, timestamp, defaultPrevented()
+        //target,toElement,srcElement,currentTarget
+        //clientX,clientY,screenX,screenY
+        //altKey,ctrlKey,shiftKEY,keyCode
+
+        //before or similar like addEventListner()
+        // attachEvent()
+        // jQuery - on
